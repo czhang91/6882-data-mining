f6a3ca4630fb11a4aa171725989a7ea05cd33e00
Hongkun LIU <66795576+lhkun123@users.noreply.github.com>
2024-04-07 21:08:49 -0700

    Lasso Regression with normalization

--- GB.py
+++ GB.py
@@ -3,6 +3,7 @@ from sklearn.ensemble import GradientBoostingRegressor
 from sklearn.linear_model import LinearRegression, Lasso, Ridge
 
 import matplotlib
+from sklearn.preprocessing import MinMaxScaler
 from sklearn.tree import DecisionTreeRegressor
 
 matplotlib.use('TkAgg')
@@ -11,11 +12,15 @@ data = pd.read_csv("Sub_Oil_VLCC_Monthly.csv")
 data = data.drop(data.columns[0], axis=1)
 
 X = data.drop('67321', axis=1).iloc[:-1]
+scaler = MinMaxScaler()
+X = scaler.fit_transform(X)
+
 Y = data[:]['67321'].iloc[1:]
 X_train = X.iloc[:-36]  # 36 is the size of test sample#
 X_test = X.iloc[-36:]
 Y_train = Y.iloc[:-36]
 Y_test = Y.iloc[-36:]
+
 
 gradient_boosting_regressor = GradientBoostingRegressor(n_estimators=100, learning_rate=0.1, max_depth=3, random_state=42)
 
