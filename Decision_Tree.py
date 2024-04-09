0eabd3097f86e5bf15bffe14f1ef236111241a19
Hongkun LIU <66795576+lhkun123@users.noreply.github.com>
2024-04-08 21:16:04 -0700

    Accuracy result for new target ids

---
+++ Decision_Tree.py
@@ -0,0 +1,44 @@
+import pandas as pd
+from sklearn.ensemble import GradientBoostingRegressor
+from sklearn.linear_model import LinearRegression, Lasso, Ridge
+
+import matplotlib
+from sklearn.neural_network import MLPRegressor
+from sklearn.svm import SVR
+from sklearn.tree import DecisionTreeRegressor
+
+matplotlib.use('TkAgg')
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("Sub_Oil_VLCC_Monthly.csv")
+data = data.drop(data.columns[0], axis=1)
+
+X = data.drop('541982', axis=1).iloc[:-1]
+Y = data[:]['541982'].iloc[1:]
+X_train = X.iloc[:-36]  # 36 is the size of test sample#
+X_test = X.iloc[-36:]
+Y_train = Y.iloc[:-36]
+Y_test = Y.iloc[-36:]
+
+# mlp_regressor = MLPRegressor(hidden_layer_sizes=(100,), activation='relu', solver='adam', max_iter=1000, random_state=42)
+#
+# # Fit the model to our training data
+# mlp_regressor.fit(X_train, Y_train)
+#
+# # Predict using the test set
+# Y_pred = mlp_regressor.predict(X_test)
+decision_tree_regressor = DecisionTreeRegressor(max_depth=5)
+
+# Fit the model to the training data
+decision_tree_regressor.fit(X_train, Y_train)
+Y_pred = decision_tree_regressor.predict(X_test)
+
+
+for item in Y_pred:
+    print(item)
+print("________")
+for i, item in enumerate(Y_test):
+    print(100 * (1 - abs(item - Y_pred[i]) / item))
+
+accuracy = 100 * (1 - abs((Y_test - Y_pred) / Y_test))
+print(accuracy.mean())
